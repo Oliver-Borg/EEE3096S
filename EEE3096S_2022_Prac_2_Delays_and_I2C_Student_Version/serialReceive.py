d065001852b75ebf390283b3321d0e675920ff12
Oliver Borg <otrolie@gmail.com>
2022-09-26 15:17:10 +0200

    Added additional comments to the python script

--- EEE3096S_2022_Prac_2_Delays_and_I2C_Student_Version/serialReceive.py
+++ EEE3096S_2022_Prac_2_Delays_and_I2C_Student_Version/serialReceive.py
@@ -10,7 +10,7 @@ timeout = 10
 
 t1 = time.time()
 
-
+# Try to connect to the serial port until a set timeout period has been reached
 while(time.time()-t1 < timeout):
     try:
         serialPort = serial.Serial(port = "COM3", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
@@ -32,6 +32,8 @@ received = 0
 
 full_received = 0
 
+
+# Wait until Start is received and just simply output the messages in the same way as putty
 while(1):
     # Wait until there is data waiting in the serial buffer
     if(serialPort.in_waiting > 0):
@@ -41,24 +43,27 @@ while(1):
         if serialString.replace('\x00', '').strip() == "Start":
             break
 
+# Reset the timer
 t1 = time.time()
 while(1):
     # Wait until there is data waiting in the serial buffer
     if(serialPort.in_waiting > 0):
         # Read data out of the buffer until a carraige return / new line is found
         serialString = serialPort.readline().decode('Ascii').replace('\x00', '').strip()
+        # If we receive Start again it means the reset button has been pressed so we reset the counters and timer
         if serialString == "Start":
             received = 0
             full_received = 0
             t1 = time.time()
             continue
-        # Print the contents of the serial data
+        # Every 3 messages (epoch time, date, time) clear the screen and display the message statistics
         if received % 3 == 0 and received > 0:
             full_received += 1
             os.system('cls')       
             tdelta = time.time()-t1
             if full_received > 0:
                 print("Received {} messages in {} seconds. ({:.5f}s/m)".format(full_received, tdelta, tdelta/full_received))
+        # Display a single message
         print(serialString)
         if(serialString != ""):
             received += 1
